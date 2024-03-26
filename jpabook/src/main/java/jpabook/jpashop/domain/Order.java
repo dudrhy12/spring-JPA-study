cbb393b75962d34b2b0de8b5fd66ccc2ebc45b05
youngseo cho <dudtj0412@naver.com>
2024-03-26 18:10:33 +0900

    Feat: 섹션4 실습코드
    실전 예제 1 - 요구사항 분석과 기본 매핑

---
+++ jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ORDERS")
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name="ORDER_ID")
+    private Long id;
+    @Column(name="MEMBER_ID")
+    private Long memberId;
+    private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
