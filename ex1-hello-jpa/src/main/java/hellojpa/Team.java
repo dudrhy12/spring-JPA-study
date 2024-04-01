0f9b1e0347735d5c39ca84300fb4077432f4b85b
youngseo cho <dudtj0412@naver.com>
2024-04-01 16:19:26 +0900

    섹션7: @MappedSuperclass

--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 @Entity
-public class Team {
+public class Team extends BaseEntity {
     @Id @GeneratedValue
     @Column(name="TEAM_ID")
     private Long id;
