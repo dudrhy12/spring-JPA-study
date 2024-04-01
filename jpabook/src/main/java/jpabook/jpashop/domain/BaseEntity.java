196ef0278c69a12fa243cd9df002be25a9a59589
youngseo cho <dudtj0412@naver.com>
2024-04-01 16:44:21 +0900

    섹션7: 실전 예제 4 -상속관계 매핑
    - 조인 전략 (고급매핑)
    - BaseEntity를 이용한 @MappedSuperclass

---
+++ jpabook/src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name="UNSERT_MEMBER")
+    private String createdBy;
+    private LocalDateTime localDateTime;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
