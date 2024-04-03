4c62480f2f8b96622392ba621524a3e6793adcdd
youngseo cho <dudtj0412@naver.com>
2024-04-03 12:57:51 +0900

    섹션9: 실전예제6-값타입매핑
    - @Embeddable, @Embedded
    - 객체지향적 설계를 위한 추가 메소드

--- jpabook/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpabook/src/main/java/jpabook/jpashop/domain/Member.java
@@ -11,14 +11,29 @@ public class Member extends BaseEntity {
     @Column(name="MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 
     public void setId(Long id) {
@@ -33,27 +48,4 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
 }
