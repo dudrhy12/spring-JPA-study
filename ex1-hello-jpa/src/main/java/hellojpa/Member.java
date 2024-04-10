dc0ef99a1ee1a0983d6299cb57a342eb31a1d4b4
영서 <dudrhy12@sookmyung.ac.kr>
2024-04-11 00:08:47 +0900

    섹션11: Named 쿼리

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,6 +6,10 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member extends BaseEntity{
     @Id
     @GeneratedValue
